b49fc66eeed046589f5f7cb175b6d11150666031
Prince Nmezi <stephenprince427@gmail.com>
2024-01-25 01:27:14 +0100

    folder structure <mvc pattern>

---
+++ src/test/java/com/springboot/blog/blogrestapi/BlogRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
